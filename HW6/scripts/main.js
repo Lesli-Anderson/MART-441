e3de648f46d92a0ad990a0abda8c6d73e1d016b9
Lesli Anderson <98143207+Lesli-Anderson@users.noreply.github.com>
2022-02-28 19:51:10 -0700

    Update main.js

--- HW6/scripts/main.js
+++ HW6/scripts/main.js
@@ -1,8 +1,15 @@
-// create an array of image names that correspond to the image tags
+// this is an array of image names that correspond to the image tags
 var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8"];
-// create a variable with the blank image name
-var blankImagePath = "images/O'Keeffe_Portrait_SquareSmall.png";
-// create a empty array for the actual images
+// variable with the blank image name
+var blankImagePath = "images/OKeefe_Portrait.png";
+var fristNumber = -1;
+var secondNumber = -1;
+var score = 0;
+// Here is JSON declaration
+var player = {"firstname":"", "lastname":"", "age":0, "score":0};
+
+
+// empty array for the actual images
 var actualImages = new Array();
 
     
@@ -11,7 +18,7 @@ function printBlanks()
    // call our random image creation function
     createRandomImageArray();
     // create a for loop
-    for(var i = 0; i < imageTags.length; ++i)
+    for(var i = 0; i < imageTags.length; i++)
     {
     // iterate through the image tag ids and sets the source 
         document.getElementById(imageTags[i]).src= blankImagePath;
@@ -27,12 +34,13 @@ function createRandomImageArray()
     var actualImagePath = ["images/BlueFlowerOKeeffe.png", "images/CallaLilyTurnedAwayOKeeffe.png", "images/RamesHeadBlueMorningGloryOKeeffe.jpg", "images/CannaLeavesOKeeffe.png"];
     // create another array to make sure the images only get added twice
     var count = [0,0];
+
     // create a while statement to check to see if our actual image array is full
     while(actualImages.length < 8)
     {
         // get a random number between 0 and the number total number of images that we can choose from
         var randomNumber = Math.floor(Math.random() * actualImagePath.length)
-          // create an if statement that says if the total number added is less than 2, then
+          //if statement that says if the total number added is less than 2, then
         // add the image to the actual image array
         if(count[randomNumber] < 2)
         {

9d9779db8283c3b5c1883468c1be74cbb71c5c73
Lesli Anderson <98143207+Lesli-Anderson@users.noreply.github.com>
2022-05-02 12:25:48 -0600

    Update main.js

--- HW15/main.js
+++ HW15/main.js
@@ -1,2 +1,18 @@
-/* Hello, World! program in node.js */
-console.log("Hello, World!");
+var fs = require("fs");
+
+console.log("Going to delete directory /tmp/test");
+fs.rmdir("/tmp/test", function (err) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log("Going to read directory /tmp");
+
+  fs.readdir("/tmp/", function (err, files) {
+    if (err) {
+      return console.error(err);
+    }
+    files.forEach(function (file) {
+      console.log(file);
+    });
+  });
+});

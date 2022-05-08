44bef88e6b55bc877eac2f1de958b1bf989378a3
Lesli Anderson <98143207+Lesli-Anderson@users.noreply.github.com>
2022-05-08 02:00:50 -0600

    Update canvas.js

--- HW16/canvas.js
+++ HW16/canvas.js
@@ -49,12 +49,20 @@ window.addEventListener("mousemove", function (event) {
   mouse.y = event.y;
 });
 
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  init();
+});
+
 function Circle(x, y, dx, dy, radius) {
   this.x = x;
   this.y = y;
   this.dx = dx;
   this.dy = dy;
   this.radius = radius;
+  this.minRadius = minRadius;
   this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
   this.draw = function () {
@@ -83,25 +91,27 @@ function Circle(x, y, dx, dy, radius) {
       if (this.radius < maxRadius) {
         this.radius += 1;
       }
-    } else if (this.radius > minRadius) {
+    } else if (this.radius > this.minRadius) {
       this.radius -= 1;
     }
 
     this.draw();
   };
 }
-
 var circleArray = [];
+function init() {
+  circleArray = [];
+  for (var i = 0; i < 800; i++) {
+    var radius = Math.random() * 3 + 1;
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var dx = Math.random() - 0.5;
+    var dy = Math.random() - 0.5;
 
-for (var i = 0; i < 100; i++) {
-  var radius = 30;
-  var x = Math.random() * (innerWidth - radius * 2) + radius;
-  var y = Math.random() * (innerHeight - radius * 2) + radius;
-  var dx = Math.random() - 0.5;
-  var dy = Math.random() - 0.5;
-
-  circleArray.push(new Circle(x, y, dx, dy, radius));
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+  }
 }
+init();
 
 function animate() {
   requestAnimationFrame(animate);

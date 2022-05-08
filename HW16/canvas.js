b08e76bfd1b8291fca8460ad5fb183486a861aa9
Lesli Anderson <98143207+Lesli-Anderson@users.noreply.github.com>
2022-05-08 00:55:17 -0600

    Create canvas.js

---
+++ HW16/canvas.js
@@ -0,0 +1,87 @@
+var canvas = document.querySelector("canvas");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext("2d");
+
+//Squares
+//c.fillStyle = "#501f3a";
+//c.fillRect(100, 100, 100, 100);
+//c.fillStyle = "#cb2d6f";
+//c.fillRect(400, 100, 100, 100);
+//c.fillStyle = "#14a098";
+//c.fillRect(300, 300, 100, 100);
+
+//Line
+//c.beginPath();
+//c.moveTo(50, 300);
+//c.lineTo(300, 100);
+//c.lineTo(400, 300);
+//c.strokeStyle = "#cccccc";
+//c.stroke();
+
+//Arc / Circle
+//c.beginPath();
+//c.arc(300, 300, 30, 0, (Math.PI = 7), false);
+//c.strokeStyle = "#cb3d6f";
+//c.stroke();
+
+//for (var i = 0; i < 1; i++) {
+//var x = Math.random() * window.innerWidth;
+//var y = Math.random() * window.innerHeight;
+//c.beginPath();
+// c.arc(x, y, 30, 0, (Math.PI = 7), false);
+// c.strokeStyle = "#cb3d6f";
+//c.stroke();
+//}
+
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+
+  this.draw = function () {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, (Math.PI = 7), false);
+    c.strokeStyle = "#cb2d6f";
+    c.stroke();
+  };
+  this.update = function () {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+  };
+}
+
+var circleArray = [];
+
+for (var i = 0; i < 100; i++) {
+  var radius = 30;
+  var x = Math.random() * innerWidth;
+  var y = Math.random() * innerHeight;
+  var dx = Math.random() - 0.5;
+  var dy = Math.random() - 0.5;
+
+  circleArray.push(new Circle(x, y, dx, dy, radius));
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
+
+  for (var i = 0; i < circleArray.length; i++) {
+    circleArray[i].update();
+  }
+}
+
+animate();
